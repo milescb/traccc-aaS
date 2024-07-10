ecc2e6bb873d72e3c47ae21abbc6df57028c9edf
milescb <mcochranbranson@gmail.com>
2024-07-10 15:42:30 +0000

    update to use csv

--- performance/README.md
+++ performance/README.md
@@ -13,15 +13,23 @@ python generate_json.py -i <input_file> -o <output_file>
 Run `perf_analyzer` with the following:
 
 ```
-perf_analyzer -m traccc-gpu --input-data $DATADIR/../test_data/test_perf_data.json \
-    --concurrency-range <start>:<end>:<step> &> <log_dir>/<log_name>
+perf_analyzer -m traccc-gpu --input-data $DATADIR/../test_data/test_perf_data.json
 ```
 
 ## Performance Plots
 
 ### Throughput as a function of concurrency
 
-Run `perf_analyzer` with the CPU and GPU configuration, then run
+Run `perf_analyzer` with the CPU and GPU configuration
+
+```
+perf_analyzer -m traccc-gpu --input-data $DATADIR/../test_data/test_perf_data.json \
+    --concurrency-range <start>:<end>:<step> &> <log_dir>/<log_name> \
+    -f out_gpu.csv --verbose-csv --collect-metrics \
+    --measurement-interval 10000
+```
+
+then run
 
 ```
 python plot_concurrency_studies.py
@@ -33,8 +41,9 @@ First, make `.log` files containing the output of running the `traccc` examples 
 
 ```
 $INSTALLDIR/bin/traccc_seq_example_cuda \
+    --use-detray-detector \
     --detector-file=$DATADIR/tml_detector/trackml-detector.csv \
-    --digitization-file=$DATADIR/tml_detector/default-geometric-config-generic.json 
+    --digitization-file=$DATADIR/tml_detector/default-geometric-config-generic.json \
     --input-directory=$DATADIR/tml_full/ttbar_mu100 &> data/logs/gpu_mu100.log
 ```
 
--- performance/plot_concurrency_studies.py
+++ performance/plot_concurrency_studies.py
@@ -1,66 +1,58 @@
-import re
+import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
-def extract_data_from_text(file_path):
-    data = {'concurrency': [], 'throughput': [], 'latency': []}
-    
-    with open(file_path, 'r') as file:
-        content = file.read()
-    
-    # Define the regex patterns
-    concurrency_pattern = re.compile(r'Request concurrency: (\d+)')
-    throughput_pattern = re.compile(r'Throughput: ([\d.]+) infer/sec')
-    latency_pattern = re.compile(r'Avg latency: (\d+) usec')
-    
-    # Find all matches
-    concurrencies = concurrency_pattern.findall(content)
-    throughputs = throughput_pattern.findall(content)
-    latencies = latency_pattern.findall(content)
-    
-    # Convert string matches to appropriate types and store in the dictionary
-    for concurrency, throughput, latency in zip(concurrencies, throughputs, latencies):
-        data['concurrency'].append(int(concurrency))
-        data['throughput'].append(float(throughput))
-        data['latency'].append(int(latency))
-    
-    return data
-
 def plot_data(cpu_data, gpu_data, 
-              variable='throughput',
+              variable='Inferences/Second',
               ylabel='Throughput (infer/sec)',
               save_path='plots',
-              save_name='concurrency_vs_throughput.png'):
+              save_name='concurrency_vs_throughput.pdf'):
     
     plt.figure(figsize=(5, 5))
-    plt.plot(cpu_data['concurrency'], cpu_data[variable], 'o-', label='CPU')
-    plt.plot(gpu_data['concurrency'], gpu_data[variable], 's-', label='GPU')
+    plt.plot(cpu_data['Concurrency'].values, cpu_data[variable].values, 'o-', label='CPU')
+    plt.plot(gpu_data['Concurrency'].values, gpu_data[variable].values, 's-', label='GPU')
     plt.xlabel('Concurrency', loc='right')
     plt.ylabel(ylabel, loc='top')
     plt.legend()
     plt.grid(True)
     
-    plt.savefig(f'{save_path}/{save_name}.pdf')
+    plt.savefig(f'{save_path}/{save_name}', bbox_inches='tight')
+    
+def gpu_to_number(df):
+    for column in df.columns:
+        if 'GPU' in column:
+            df[column] = pd.to_numeric(df[column].str.split(':').str[-1].str.rstrip(';'), errors='coerce')
+    return df
 
 def main():
     plot_data(cpu_data, gpu_data)
     plot_data(cpu_data, gpu_data, 
-              variable='latency', 
+              variable='Avg latency', 
               ylabel='Latency (usec)', 
-              save_name='concurrency_vs_latency.png')
+              save_name='concurrency_vs_latency.pdf')
+    plot_data(cpu_data, gpu_data, 
+              variable='Avg GPU Utilization', 
+              ylabel='Average GPU Utilization (%)',
+              save_name='concurrency_vs_gpu_utilization.pdf')
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-igpu", "--input-gpu", 
-                        default='data/gpu_concurrency_vs_throughput.log',
+    parser.add_argument("-gpu", "--input-gpu", 
+                        default='data/out_gpu.csv',
                         type=str, help="Input text file path")
-    parser.add_argument("-icpu", "--input-cpu",
-                        default='data/cpu_concurrency_vs_throughput.log',
+    parser.add_argument("-cpu", "--input-cpu",
+                        default='data/out_cpu.csv',
                         type=str, help="Input text file path")
     args = parser.parse_args()
     
-    cpu_data = extract_data_from_text(args.input_cpu)
-    gpu_data = extract_data_from_text(args.input_gpu)
+    cpu_data = pd.read_csv(args.input_cpu)
+    gpu_data = pd.read_csv(args.input_gpu)
+    
+    cpu_data = cpu_data.sort_values(by='Concurrency', ascending=True)
+    gpu_data = gpu_data.sort_values(by='Concurrency', ascending=True)
+    
+    cpu_data = gpu_to_number(cpu_data)
+    gpu_data = gpu_to_number(gpu_data)
     
     main()
